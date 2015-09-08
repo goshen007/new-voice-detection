7d139946fc730033643346ce69d3a4f89bbf79e7
jiemojiemo <460706836@qq.com>
2015-09-08 16:36:40 +0800

    add pcm2wav

--- newvoicedetection/pcm2wav.cpp
+++ newvoicedetection/pcm2wav.cpp
@@ -4,324 +4,204 @@
 #include <Windows.h>
 #include "pcm2wav.h"
 
-CPcm2Wav::CPcm2Wav()
+CPcm2Wav::CPcm2Wav(const string& inFile, const string& outFile):
+	m_inFileName(inFile), m_outFileName(outFile), m_isFile2File(true)
+{
+}
+
+CPcm2Wav::CPcm2Wav( float* buffer, int bufferSize, const string& outFileName):
+	m_outFileName(outFileName), m_pbuffer((void*)buffer),m_bufferSize(bufferSize),m_isFile2File(false)
 {
 
 }
 
-int CPcm2Wav::Pcm2Wav( const float* buffer, unsigned long bufferSize,const Pcm2WavInfo& info )
+CPcm2Wav::~CPcm2Wav()
 {
-	const string out( info.outFileName );
-	int channel = info.channels;
-	int sample_rate = info.sampleRate;
-	int sample_bits = info.sampleBits;
-	int format_tag = info.formatTag;	
-	char word[5];
-	DWORD DWORD_num;		
-	WORD WORD_num;
-	FILE *fpD;
-	unsigned long dataSize = 0;
-
-	fpD = fopen(out.c_str(),"wb");
-
-	dataSize = bufferSize;
-
-	//写RIFF
-	fseek( fpD,RIFFID ,SEEK_SET );
-	strcpy(word,"RIFF");
-	fwrite( word, 1, 4, fpD );
-
-	//.....写CHUNKE_SIZE = 音频实际大小 + 文件头（44个字节） - 8
-	DWORD_num = dataSize + 0x2c - 8;
-	fseek( fpD, CHUNKE_SIZE, SEEK_SET );
-	fwrite( &DWORD_num, 1, sizeof(DWORD), fpD );
-
-	//写WAVE
-	strcpy(word,"WAVE");
-	fseek( fpD, WAVE, SEEK_SET );
-	fwrite( word, 1, 4, fpD );
-
-	//写FMT
-	strcpy(word,"fmt ");
-	fseek( fpD, FMT, SEEK_SET );
-	fwrite( word, 1, 4, fpD );
-
-	//写size(16 or 18)通常16
-	DWORD_num = 16;
-	fseek( fpD, FMT_CKSIZE, SEEK_SET );
-	fwrite( &DWORD_num, 1, sizeof(DWORD), fpD );
-
-	//写AudioFormat
-	WORD_num = format_tag;	//pcm数据
-	fseek( fpD, AUDIO_FORMAT, SEEK_SET );
-	fwrite( &WORD_num, 1, sizeof(WORD), fpD );
-
-	//写声道数
-	WORD_num = channel;
-	fseek( fpD, CHANNELS, SEEK_SET );
-	fwrite( &WORD_num, 1, sizeof(WORD), fpD );
-
-	//写采样率
-	DWORD_num = sample_rate;
-	fseek( fpD, SAMPLE_RATE, SEEK_SET );
-	fwrite( &DWORD_num, 1, sizeof(DWORD), fpD );
-
-	//写byte率 = 采样频率*音频通道数*每次采样得到的样本位数/8
-	DWORD_num = sample_rate * channel * sample_bits / 8;
-	fseek( fpD, BYTE_RATE, SEEK_SET );
-	fwrite( &DWORD_num, 1, sizeof(DWORD), fpD );
-
-	//写块对齐 块对齐=通道数*每次采样得到的样本位数/8
-	WORD_num = channel * sample_bits / 8;
-	fseek( fpD, BLOCKALIGN, SEEK_SET );
-	fwrite( &WORD_num, 1, sizeof(WORD), fpD );
-
-	//写样本数据位
-	WORD_num = sample_bits;
-	fseek( fpD, BITS_PER_SAMPLE, SEEK_SET );
-	fwrite( &WORD_num, 1, sizeof(WORD), fpD );
-
-	//写"DATA"
-	strcpy( word, "data" );
-	fseek( fpD, DATAID, SEEK_SET );
-	fwrite( word, 1, 4, fpD );
-
-
-	//写实际的音频数据
-	fseek( fpD, DATA_BEGIN, SEEK_SET );
-	fwrite( buffer, 1, dataSize, fpD ); 
-
-	fclose( fpD );
-
-	return 0;
-}
-int CPcm2Wav::Pcm2Wav( const Pcm2WavInfo& info )
-{
-	const string in( info.inFileName );
-	const string out( info.outFileName );
-	int channel = info.channels;
-	int sample_rate = info.sampleRate;
-	int sample_bits = info.sampleBits;
-	int format_tag = info.formatTag;	
-	char word[5];
-	DWORD DWORD_num;		
-	WORD WORD_num;
-	FILE *fpS;
-	FILE *fpD;
-	int dataSize = 0;
-	float* dataBuffer = NULL;
-
-	fpS = fopen(in.c_str(),"rb");
-	fpD = fopen(out.c_str(),"wb");
-
-
-	//将文件指针指向文件尾巴
-	fseek( fpS, 0, SEEK_END );
-	//获取音频数据的实际大小
-	dataSize = ftell( fpS );
-	//还原文件指针位置
-	fseek( fpS, 0, SEEK_SET );
-
-	//写RIFF
-	fseek( fpD,RIFFID ,SEEK_SET );
-	strcpy(word,"RIFF");
-	fwrite( word, 1, 4, fpD );
-
-	//.....写CHUNKE_SIZE = 音频实际大小 + 文件头（44个字节） - 8
-	DWORD_num = dataSize + 0x2c - 8;
-	fseek( fpD, CHUNKE_SIZE, SEEK_SET );
-	fwrite( &DWORD_num, 1, sizeof(DWORD), fpD );
-
-	//写WAVE
-	strcpy(word,"WAVE");
-	fseek( fpD, WAVE, SEEK_SET );
-	fwrite( word, 1, 4, fpD );
-
-	//写FMT
-	strcpy(word,"fmt ");
-	fseek( fpD, FMT, SEEK_SET );
-	fwrite( word, 1, 4, fpD );
-
-	//写size(16 or 18)通常16
-	DWORD_num = 16;
-	fseek( fpD, FMT_CKSIZE, SEEK_SET );
-	fwrite( &DWORD_num, 1, sizeof(DWORD), fpD );
-
-	//写AudioFormat
-	WORD_num = format_tag;	//pcm数据
-	fseek( fpD, AUDIO_FORMAT, SEEK_SET );
-	fwrite( &WORD_num, 1, sizeof(WORD), fpD );
-
-	//写声道数
-	WORD_num = channel;
-	fseek( fpD, CHANNELS, SEEK_SET );
-	fwrite( &WORD_num, 1, sizeof(WORD), fpD );
-
-	//写采样率
-	DWORD_num = sample_rate;
-	fseek( fpD, SAMPLE_RATE, SEEK_SET );
-	fwrite( &DWORD_num, 1, sizeof(DWORD), fpD );
-
-	//写byte率 = 采样频率*音频通道数*每次采样得到的样本位数/8
-	DWORD_num = sample_rate * channel * sample_bits / 8;
-	fseek( fpD, BYTE_RATE, SEEK_SET );
-	fwrite( &DWORD_num, 1, sizeof(DWORD), fpD );
-
-	//写块对齐 块对齐=通道数*每次采样得到的样本位数/8
-	WORD_num = channel * sample_bits / 8;
-	fseek( fpD, BLOCKALIGN, SEEK_SET );
-	fwrite( &WORD_num, 1, sizeof(WORD), fpD );
-
-	//写样本数据位
-	WORD_num = sample_bits;
-	fseek( fpD, BITS_PER_SAMPLE, SEEK_SET );
-	fwrite( &WORD_num, 1, sizeof(WORD), fpD );
-
-	//写"DATA"
-	strcpy( word, "data" );
-	fseek( fpD, DATAID, SEEK_SET );
-	fwrite( word, 1, 4, fpD );
-
-	//写实际音频数据大小
-	DWORD_num = dataSize;
-	fseek( fpD, DATA_SIZE, SEEK_SET );
-	fwrite( &DWORD_num, 1, sizeof(DWORD), fpD );
-
-	//写实际的音频数据
-
-	//从文件读取音频数据
-	dataBuffer = (float*)malloc( dataSize );
-	assert( dataBuffer != NULL );
-	fseek( fpS, 0, SEEK_SET );
-	fread( dataBuffer, 1, dataSize, fpS );
-
-	fseek( fpD, DATA_BEGIN, SEEK_SET );
-	fwrite( dataBuffer, 1, dataSize, fpD ); 
-
-	if( dataBuffer != NULL )
+	//如果是 ‘文件’->'wav文件'，需要将输入文件的内容拷贝至m_pbuffer中
+	//所以在这里进行释放
+	//否则是 'buffer' -> ‘wav文件’，不需要释放
+	if (m_isFile2File)
 	{
-		free( dataBuffer );
-		dataBuffer = NULL;
+		free(m_pbuffer);
 	}
-
-	fclose( fpS );
-	fclose( fpD );
-
-	return 0;
 }
 
-int CPcm2Wav::_Pcm2Wav( string inFile, string outFile, int channels, int sam_rate, int sam_bits, int fmt_tag )
+int CPcm2Wav::Pcm2Wav(const Pcm2WavParameter& para)
 {
-	const string in( inFile );
-	const string out( outFile );
-	int channel = channels;
-	int sample_rate = sam_rate;
-	int sample_bits = sam_bits;
-	int format_tag = fmt_tag;	
-	char word[5];
-	DWORD DWORD_num;		
-	WORD WORD_num;
-	FILE *fpS;
-	FILE *fpD;
-	int dataSize = 0;
-	float* dataBuffer = NULL;
-
-	fpS = fopen(in.c_str(),"rb");
-	fpD = fopen(out.c_str(),"wb");
-
-
-	//将文件指针指向文件尾巴
-	fseek( fpS, 0, SEEK_END );
-	//获取音频数据的实际大小
-	dataSize = ftell( fpS );
-	//还原文件指针位置
-	fseek( fpS, 0, SEEK_SET );
-
-	//写RIFF
-	fseek( fpD,RIFFID ,SEEK_SET );
-	strcpy(word,"RIFF");
-	fwrite( word, 1, 4, fpD );
-
-	//.....写CHUNKE_SIZE = 音频实际大小 + 文件头（44个字节） - 8
-	DWORD_num = dataSize + 0x2c - 8;
-	fseek( fpD, CHUNKE_SIZE, SEEK_SET );
-	fwrite( &DWORD_num, 1, sizeof(DWORD), fpD );
-
-	//写WAVE
-	strcpy(word,"WAVE");
-	fseek( fpD, WAVE, SEEK_SET );
-	fwrite( word, 1, 4, fpD );
-
-	//写FMT
-	strcpy(word,"fmt ");
-	fseek( fpD, FMT, SEEK_SET );
-	fwrite( word, 1, 4, fpD );
-
-	//写size(16 or 18)通常16
-	DWORD_num = 16;
-	fseek( fpD, FMT_CKSIZE, SEEK_SET );
-	fwrite( &DWORD_num, 1, sizeof(DWORD), fpD );
-
-	//写AudioFormat
-	WORD_num = format_tag;	//pcm数据
-	fseek( fpD, AUDIO_FORMAT, SEEK_SET );
-	fwrite( &WORD_num, 1, sizeof(WORD), fpD );
-
-	//写声道数
-	WORD_num = channel;
-	fseek( fpD, CHANNELS, SEEK_SET );
-	fwrite( &WORD_num, 1, sizeof(WORD), fpD );
-
-	//写采样率
-	DWORD_num = sample_rate;
-	fseek( fpD, SAMPLE_RATE, SEEK_SET );
-	fwrite( &DWORD_num, 1, sizeof(DWORD), fpD );
-
-	//写byte率 = 采样频率*音频通道数*每次采样得到的样本位数/8
-	DWORD_num = sample_rate * channel * sample_bits / 8;
-	fseek( fpD, BYTE_RATE, SEEK_SET );
-	fwrite( &DWORD_num, 1, sizeof(DWORD), fpD );
-
-	//写块对齐 块对齐=通道数*每次采样得到的样本位数/8
-	WORD_num = channel * sample_bits / 8;
-	fseek( fpD, BLOCKALIGN, SEEK_SET );
-	fwrite( &WORD_num, 1, sizeof(WORD), fpD );
-
-	//写样本数据位
-	WORD_num = sample_bits;
-	fseek( fpD, BITS_PER_SAMPLE, SEEK_SET );
-	fwrite( &WORD_num, 1, sizeof(WORD), fpD );
-
-	//写"DATA"
-	strcpy( word, "data" );
-	fseek( fpD, DATAID, SEEK_SET );
-	fwrite( word, 1, 4, fpD );
-
-	//写实际音频数据大小
-	DWORD_num = dataSize;
-	fseek( fpD, DATA_SIZE, SEEK_SET );
-	fwrite( &DWORD_num, 1, sizeof(DWORD), fpD );
-
-	//写实际的音频数据
-
-	//从文件读取音频数据
-	dataBuffer = (float*)malloc( dataSize );
-	assert( dataBuffer != NULL );
-	fseek( fpS, 0, SEEK_SET );
-	fread( dataBuffer, 1, dataSize, fpS );
-
-	fseek( fpD, DATA_BEGIN, SEEK_SET );
-	fwrite( dataBuffer, 1, dataSize, fpD ); 
-
-	if( dataBuffer != NULL )
+	assert( para.channels == 1 || para.channels == 2 );
+	assert( para.formatTag > 0 );
+	assert( para.sampleBits==8 || para.sampleBits==16 || para.sampleBits==24 | para.sampleBits==32);
+	assert( para.sampleRate > 0 );
+	m_parameter = para;
+	if (m_isFile2File)
 	{
-		free( dataBuffer );
-		dataBuffer = NULL;
+		Pcm2Wav_File2File();
 	}
-
-	fclose( fpS );
-	fclose( fpD );
-
+	else
+	{
+		Pcm2Wav_Buffer2File();
+	}
 	return 0;
+}
+void CPcm2Wav::Pcm2Wav_File2File()
+{
+	// ‘文件’-》‘内存’
+	ReadFileToMemory();
+	Pcm2Wav_Buffer2File();
+}
+void CPcm2Wav::Pcm2Wav_Buffer2File()
+{
+	//初始化输出的参数
+	m_outFileIndex = 0;
+	OpenOutputFile();
+	WriteToOutputFile();
+	CloseOutputFile();
+}
+void CPcm2Wav::ReadFileToMemory()
+{
+	m_pInFile = fopen(m_inFileName.c_str(), "rb");
+	if (m_pInFile == NULL)
+	{
+		fprintf(stderr, "Eorror:Cannnot open the input file.\n");
+		exit(-1);
+	}
+	//读入文件内容
+	fseek(m_pInFile, 0, SEEK_END);
+	m_bufferSize = ftell(m_pInFile);
+	fseek(m_pInFile, 0, SEEK_SET);
+	m_pbuffer = malloc(m_bufferSize);
+	fread(m_pbuffer, 1, m_bufferSize, m_pInFile);
+	fclose(m_pInFile);
+}
+void CPcm2Wav::OpenOutputFile()
+{
+	m_pOutFile = fopen(m_outFileName.c_str(), "wb");
+	if (m_pOutFile == NULL)
+	{
+		fprintf(stderr, "Eorror:Cannnot open the output file.\n");
+		exit(-1);
+	}
+}
+void CPcm2Wav::WriteToOutputFile()
+{
+	WriteRIFFChunk();
+	WriteFMTChunk();
+	WriteDataChunk();
+}
+void CPcm2Wav::WriteSomeDataToOutFile( const void* str, int size )
+{
+	fseek( m_pOutFile, m_outFileIndex, SEEK_SET );
+	fwrite( str, 1, size, m_pOutFile);
+	m_outFileIndex += size;
+}
+unsigned long CPcm2Wav::GetDataFileSize()
+{
+	return m_bufferSize;
+}
+
+
+void CPcm2Wav::WriteRIFFChunk()
+{
+	WriteRIFFID();
+	WriteRIFFChunkSize();
+	WriteWAVEID();
+}
+void CPcm2Wav::WriteRIFFID()
+{
+	const char *riff = "RIFF";
+	WriteSomeDataToOutFile( riff, DWORD_SIZE );
+
+}
+void CPcm2Wav::WriteRIFFChunkSize()
+{
+	//写CHUNKE_SIZE = 音频实际大小 + 文件头（44个字节） - 8
+	 DWORD DWORD_num = GetDataFileSize() + 0x2c - 8;
+	 WriteSomeDataToOutFile( &DWORD_num, DWORD_SIZE );
+}
+void CPcm2Wav::WriteWAVEID()
+{
+	//写WAVE
+	const char* wave = "WAVE";
+	WriteSomeDataToOutFile( wave,DWORD_SIZE  );
+}
+
+void CPcm2Wav::WriteFMTChunk()
+{
+	WriteFMTID();
+	WriteFMTChunkSize();
+	WriteFormatTag();
+	WriteChannels();
+	WriteSampleRate();
+	WriteDataRate();
+	WriteBlockAlign();
+	WriteBitsPerSample();
+}
+void CPcm2Wav::WriteFMTID()
+{
+	//写FMT
+	const char* fmt = "fmt ";
+	WriteSomeDataToOutFile( fmt, DWORD_SIZE);
+}
+void CPcm2Wav::WriteFMTChunkSize()
+{
+	//写size(16 or 18)通常16
+	DWORD DWORD_num = 16;
+	WriteSomeDataToOutFile( &DWORD_num,DWORD_SIZE );
+}
+void CPcm2Wav::WriteFormatTag()
+{
+	//写AudioFormat
+	WORD WORD_num = m_parameter.formatTag;	//pcm数据
+	WriteSomeDataToOutFile( &WORD_num,WORD_SIZE );
+}
+void CPcm2Wav::WriteChannels()
+{
+	//写声道数
+	WORD WORD_num = m_parameter.channels;
+	WriteSomeDataToOutFile( &WORD_num, WORD_SIZE);
+}
+void CPcm2Wav::WriteSampleRate()
+{
+	//写采样率
+	DWORD DWORD_num = m_parameter.sampleRate;
+	WriteSomeDataToOutFile( &DWORD_num, DWORD_SIZE );
+}
+void CPcm2Wav::WriteDataRate()
+{
+	DWORD DWORD_num = m_parameter.sampleRate * m_parameter.channels * m_parameter.sampleBits / 8;
+	WriteSomeDataToOutFile( &DWORD_num, DWORD_SIZE );
+}
+void CPcm2Wav::WriteBlockAlign()
+{
+	WORD WORD_num = m_parameter.channels * m_parameter.sampleBits / 8;
+	WriteSomeDataToOutFile( &WORD_num, WORD_SIZE );
+}
+void CPcm2Wav::WriteBitsPerSample()
+{
+	WORD WORD_num = m_parameter.sampleBits;
+	WriteSomeDataToOutFile( &WORD_num, WORD_SIZE );
+}
+
+void CPcm2Wav::WriteDataChunk()
+{
+	WriteDataID();
+	WriteDataSize();
+	WriteAudioData();
+}
+void CPcm2Wav::WriteDataID()
+{
+	const char* data = "data";
+	WriteSomeDataToOutFile( data, DWORD_SIZE );
+}
+void CPcm2Wav::WriteDataSize()
+{
+	unsigned long dataSize = GetDataFileSize();
+	WriteSomeDataToOutFile( &dataSize, DWORD_SIZE );
+}
+void CPcm2Wav::WriteAudioData()
+{
+	unsigned long dataSize = GetDataFileSize();
+
+	WriteSomeDataToOutFile( m_pbuffer, dataSize );
 }
 
--- newvoicedetection/pcm2wav.h
+++ newvoicedetection/pcm2wav.h
@@ -3,6 +3,10 @@
 
 #include <string>
 using std::string;
+
+//wave格式有比较复杂的几种情况
+//这里，我们使用最简单的情况，能够支持播放器播放
+//具体复杂情况可以参照 http://blog.csdn.net/yueyihua/article/details/5494905
 //----------------以下定义一些地址
 
 //RIFF,资源交换标志
@@ -35,32 +39,71 @@ using std::string;
 //实际数据开始处
 #define DATA_BEGIN		0x2c
 
-struct Pcm2WavInfo{
+#define DWORD_SIZE		4
+#define WORD_SIZE		2
+
+struct Pcm2WavParameter{
 	unsigned int channels;		//声道数
 	unsigned int sampleRate;		//采样率
 	unsigned int sampleBits;		//采样的位数
 	unsigned int formatTag;		//格式标志
 									//3 为 32位float
 									//1 位 16位pcm
-	string		 inFileName;
-	string		 outFileName;
 };
 
 
 class CPcm2Wav{
 private:
+	CPcm2Wav() {}
 	CPcm2Wav( const CPcm2Wav& cp ){}
 	CPcm2Wav& operator=( const CPcm2Wav& cp ){}
-	int _Pcm2Wav( string inFile, string outFile, int channels, int sam_rate, int sam_bits, int fmt_tag );
-public:
-	CPcm2Wav();
+	
+	void Pcm2Wav_File2File();
+	void Pcm2Wav_Buffer2File();
+	void ReadFileToMemory();
+	void OpenOutputFile();
+	void CloseOutputFile() { fclose(m_pOutFile); }
+	void WriteToOutputFile();
+	unsigned long GetDataFileSize();
+	void WriteSomeDataToOutFile( const void* str, int size );
 
+	void WriteRIFFChunk();
+	void WriteRIFFID();
+	void WriteRIFFChunkSize();
+	void WriteWAVEID();
+
+	void WriteFMTChunk();
+	void WriteFMTID();
+	void WriteFMTChunkSize();
+	void WriteFormatTag();
+	void WriteChannels();
+	void WriteSampleRate();
+	void WriteDataRate();
+	void WriteBlockAlign();
+	void WriteBitsPerSample();
+	
+	void WriteDataChunk();
+	void WriteDataID();
+	void WriteDataSize();
+	void WriteAudioData();
+private:
+	Pcm2WavParameter m_parameter;
+	bool m_isFile2File;
+	FILE* m_pOutFile;
+	FILE* m_pInFile;
+	void* m_pbuffer;
+	unsigned long m_bufferSize;
+	int m_outFileIndex;
+	string m_inFileName;
+	string m_outFileName;
+public:
+	CPcm2Wav( const string& inFile, const string& outFile );
+	CPcm2Wav( float* buffer, int bufferSize, const string& outFileName );
+	~CPcm2Wav();
 	/**
 	* @brief : 根据infomation进行封装 
 	*/
-	int Pcm2Wav( const Pcm2WavInfo& info );
-
-	int Pcm2Wav( const float* buffer, unsigned long bufferSize,const Pcm2WavInfo& info );
+	int Pcm2Wav( const Pcm2WavParameter& info );
 };
 
 #endif
